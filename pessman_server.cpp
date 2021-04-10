2b41c91dd1adadddb47d3b3b756685780c091bc5
brandonpessman <pessman.brandon@gmail.com>
2021-04-10 17:31:21 -0500

    Test

--- pessman_server.cpp
+++ pessman_server.cpp
@@ -234,7 +234,7 @@ int main()
     cout << "Number of retransmitted packets: " << errorPackets << endl;
     cout << "Total elapsed time: " << milli_time << "ms" << endl;
     cout << "Total throughput (Mbps): "
-         << ((fileSize * 1000)) << endl;
+         << ((fileSize * 1000) / seconds) << endl;
     cout << "Effective throughput: "
          << "N/A" << endl;
 
