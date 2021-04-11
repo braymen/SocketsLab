c8ba13381c0f1287153587f6e42d33c8a723bf3c
brandonpessman <pessman.brandon@gmail.com>
2021-04-11 16:27:10 -0500

    Test

--- pessman_server.cpp
+++ pessman_server.cpp
@@ -90,7 +90,7 @@ int main()
     char ip[20] = "10.35.195.236";
     char port[20] = "9353";
     char sendFile[20];
-    int packetSize = 100;
+    int packetSize = 50;
     totalPackets = 0;
     int leftOverPacket = 0;
     int numPackets = 0;
